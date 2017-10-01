4c9e80e8d13c34608f77d0d5a4142c84ccc09002
Sergiy Sych <sychsergiy@gmail.com>
2017-10-01 21:31:33 +0300

    test csrf bug fixed

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,5 @@
+import re
+
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
@@ -8,6 +10,11 @@ from .views import home_page
 
 class HomePageTest(TestCase):
 
+	@staticmethod
+	def remove_csrf(html_code):
+		csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+		return re.sub(csrf_regex, '', html_code)
+
 	def test_root_url_resolvers_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
@@ -15,8 +22,12 @@ class HomePageTest(TestCase):
 	def test_home_page_return_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		expected_html = render_to_string('home.html')
-		self.assertEqual(response.content.decode(), expected_html)
+		expected_html = render_to_string('home.html', request=request)
+
+		self.assertEqual(
+            self.remove_csrf(response.content.decode()),
+            self.remove_csrf(expected_html)
+        )
 
 	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
@@ -29,6 +40,11 @@ class HomePageTest(TestCase):
 
 		expected_html = render_to_string(
 			'home.html',
-			{'new_item_text': 'A new list item'}
+			{'new_item_text': 'A new list item'},
+			request=request
 		)
-		self.assertEqual(response.content.decode(), expected_html)
+
+		self.assertEqual(
+            self.remove_csrf(response.content.decode()),
+            self.remove_csrf(expected_html)
+        )
