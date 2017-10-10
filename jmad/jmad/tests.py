be8736c7dfc4988cf4d0661597224a22d06f2b8d
Sergiy Sych <sychsergiy@gmail.com>
2017-10-10 12:15:10 +0300

    Adds form elements, fake search results to index.html.

--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -24,14 +24,31 @@ class StudentTestCase(LiveServerTestCase):
         # the name of the site in the heading.
         brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
         self.assertEqual('JMAD', brand_element.text)
-        self.fail('Incomplete Test')
 
-        # He sees the inputs of the search form, including
-        # labels and placeholder.
+        # He sees the inputs of the search form, including labels and
+        # placeholders.
+        instrument_input = self.browser.find_element_by_css_selector('input#jmad-instrument')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'label[for="jmad-instrument"]'))
+        self.assertEqual(instrument_input.get_attribute('placeholder'),
+                         'i.e. trumpet')
+        artist_input = self.browser.find_element_by_css_selector(
+            'input#jmad-artist')
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'label[for="jmad-artist"]'))
+        self.assertEqual(artist_input.get_attribute('placeholder'),
+                         'i.e. Davis')
 
         # He types in the name of his instrument and submits it.
+        instrument_input.send_keys('saxophone')
+        self.browser.find_element_by_css_selector('form button').click()
 
         # He sees too many search results...
+        search_results = self.browser.find_elements_by_css_selector(
+            '.jmad-search-result'
+        )
+        self.assertGreater(len(search_results), 2)
+        self.fail('Incomplete Test')
 
         # ...so he adds an artist to his search query and
         # gets a more manageable list.
