e74713ae98388b240d1f69fe820e2dc0d903f85a
Sergiy Sych <sychsergiy@gmail.com>
2017-10-10 11:59:22 +0300

    intial index template, view test

---
+++ jmad/solos/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import TestCase, RequestFactory
+
+from solos.views import index
+
+
+class IndexViewTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_index_view_basic(self):
+        """
+        Test that index view returns a 200 response and uses
+        correct template
+        """
+        request = self.factory.get('/')
+        with self.assertTemplateUsed('solos/index.html'):
+            response = index(request)
+            self.assertEqual(response.status_code, 200)
--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 
-def index():
-    pass
+def index(request):
+    return render(request, 'solos/index.html')
