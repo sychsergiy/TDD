ea119de80929678bb55e47d6aa1ff11dbf825500
Sergiy Sych <sychsergiy@gmail.com>
2017-10-11 19:55:35 +0300

    Adds str, ordering, and columns for Track admin

--- jmad/albums/models.py
+++ jmad/albums/models.py
@@ -9,9 +9,18 @@ class Album(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['name']
+
 
 class Track(models.Model):
     name = models.CharField(max_length=100)
     album = models.ForeignKey(Album)
     track_number = models.PositiveIntegerField(blank=True, null=True)
     slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['album', 'track_number']
--- jmad/jmad/tests.py
+++ jmad/jmad/tests.py
@@ -36,10 +36,16 @@ class StudentTestCase(LiveServerTestCase):
         self.solo3 = Solo.objects.create(
             instrument='saxophone', artist='Cannonball Adderley',
             track=self.track3, slug='cannonball-adderley')
-        self.track4 = Track.objects.create(name='Freddie Freeloader',
-                                           album=self.album2)
-        self.track5 = Track.objects.create(name='Blue in Green',
-                                           album=self.album2)
+        self.track4 = Track.objects.create(
+            name='Freddie Freeloader',
+            album=self.album2,
+            track_number=2
+        )
+        self.track5 = Track.objects.create(
+            name='Blue in Green',
+            album=self.album2,
+            track_number=3
+        )
 
         self.admin_user = get_user_model().objects.create_superuser(
             username='bill',
@@ -208,12 +214,27 @@ class StudentTestCase(LiveServerTestCase):
                 'My Favorite Things').get_attribute('href'),
             self.live_server_url + '/admin/albums/album/1/change/'
         )
-        self.fail('Incomplete Test')
 
         # Going back to the home page, he clicks the Tracks
         # link and sees the Tracks that have been added.
         # They're ordered first by Album, then by track
         # number.
+        self.browser.find_element_by_css_selector(
+            '#site-name a').click()
+        self.browser.find_element_by_link_text('Tracks').click()
+        track_rows = self.browser.find_elements_by_css_selector(
+            '#result_list tr')
+        self.assertEqual(track_rows[1].text,
+                         'Kind of Blue Freddie Freeloader 2')
+        self.assertEqual(track_rows[2].text,
+                         'Kind of Blue Blue in Green 3')
+        self.assertEqual(track_rows[3].text,
+                         'Kind of Blue All Blues 4')
+        self.assertEqual(track_rows[4].text,
+                         'Know What I Mean? Waltz for Debby -')
+        self.assertEqual(track_rows[5].text,
+                         'My Favorite Things My Favorite Things -')
+        self.fail('Incomplete Test')
 
         # Heads a track to an album that already exists
 
