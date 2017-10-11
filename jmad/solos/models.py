1e79c058fae2703e19f1f7cdd04d5c73a8858c65
Sergiy Sych <sychsergiy@gmail.com>
2017-10-11 09:22:32 +0300

    Refactos Solo with Album and Track, albums app

--- jmad/solos/models.py
+++ jmad/solos/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
+from albums.models import Track, Album
 
 
 class Solo(models.Model):
-    track = models.CharField(max_length=100)
+    slug = models.SlugField()
+    track = models.ForeignKey(Track)
     artist = models.CharField(max_length=100)
     instrument = models.CharField(max_length=50)
-    album = models.CharField(max_length=200)
+    album = models.ForeignKey(Album)
     start_time = models.CharField(max_length=20, blank=True, null=True)
     end_time = models.CharField(max_length=20, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('solo_detail', kwargs={
+            'album': self.track.album.slug,
+            'track': self.track.slug,
+            'artist': self.slug,
+        })
--- jmad/solos/tests/test_models.py
+++ jmad/solos/tests/test_models.py
@@ -1,10 +1,24 @@
 from django.test import TestCase
 
 from solos.models import Solo
+from albums.models import Album, Track
 
 
 class SoloModelTestCase(TestCase):
     def setUp(self):
+        self.album = Album.objects.create(
+            name='At the Stratford Shakespearean Festival',
+            artist='Oscar Peterson Trio',
+            slug='at-the-stratford-shakespearean-festival',
+        )
+
+        self.track = Track.objects.create(
+            name='Falling in Love with Love',
+            album=self.album,
+            track_number=1,
+            slug='falling-in-love-with-love',
+        )
+
         self.solo = Solo.objects.create(
             track='Falling in Love with Love',
             artist='Oscar Peterson',
@@ -20,3 +34,12 @@ class SoloModelTestCase(TestCase):
         """
         self.assertEqual(self.solo.artist, 'Oscar Peterson')
         self.assertEqual(self.solo.end_time, '4:06')
+
+    def test_get_absolute_url(self):
+        """
+        Test that we can build a URL fora solo
+        """
+        self.assertEqual(
+            self.solo.get_absolute_url(),
+            '/recordings/at-the-stratford-shakespearean-festival/falling-in-love-with-love/oscar-peterson/'
+        )
